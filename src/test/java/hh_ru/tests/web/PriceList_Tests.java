ac65b233eb71ddb56acf474e95f8c1819aaf7a2e
Admin <bogtinys20@gmail.com>
2021-03-13 13:15:02 +0300

    add Layer

--- src/test/java/hh_ru/tests/web/PriceList_Tests.java
+++ src/test/java/hh_ru/tests/web/PriceList_Tests.java
@@ -1,7 +1,9 @@
 package hh_ru.tests.web;
 
+import hh_ru.allure.Layer;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -12,6 +14,8 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Layer("web")
+@Owner("EpicFate")
 public class PriceList_Tests extends TestBase {
     @Test
     @AllureId("1840")
--- src/test/java/hh_ru/tests/web/SearchOnTheSite_Tests.java
+++ src/test/java/hh_ru/tests/web/SearchOnTheSite_Tests.java
@@ -1,7 +1,9 @@
 package hh_ru.tests.web;
 
+import hh_ru.allure.Layer;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,8 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Layer("web")
+@Owner("EpicFate")
 public class SearchOnTheSite_Tests extends TestBase {
 
     @Test
