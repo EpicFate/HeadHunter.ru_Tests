b397e2bcbb788f0b77b1c136e893469b5cb95551
Admin <bogtinys20@gmail.com>
2021-03-17 00:59:59 +0300

    fix video

--- src/test/java/hh_ru/helpers/AttachmentsHelper.java
+++ src/test/java/hh_ru/helpers/AttachmentsHelper.java
@@ -39,7 +39,7 @@ public class AttachmentsHelper {
     }
 
     public static String getVideoUrl() {
-        return "https://" + isRemoteWebDriver() + "/video/" + getSessionId() + ".mp4";
+        return "https://" + isRemoteWebDriver() + ":4444/video/" + getSessionId() + ".mp4";
     }
 
     public static String getSessionId(){
