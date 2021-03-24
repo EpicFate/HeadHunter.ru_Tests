491d642d0159d5236b88412e211b6a966f42d071
Admin <bogtinys20@gmail.com>
2021-03-24 16:19:07 +0300

    fix config

--- src/test/java/ru/hh/config/ApiConfig.java
+++ src/test/java/ru/hh/config/ApiConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:api_properties/AllRussiaToTheBasket.properties"
+        "classpath:api_properties/Basket.properties"
 })
 public interface ApiConfig extends Config {
     @Key("Body")
